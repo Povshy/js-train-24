faf889a5a67ab088d4742aea4caf9bc1584bb443
Povshy <vladcarrib@gmail.com>
2023-12-09 03:25:09 +0200

    123

--- 9.js
+++ 9.js
@@ -4,9 +4,20 @@ function task9() {
   // Створюємо функцію fetchWithError, яка симулює помилку при запиті до сервера.
   // Створюємо константу errorMessage в яку записуємо рядок "Помилка при з'єднанні з сервером"
   // Використовуємо Promise.reject(errorMessage) для створення вже відхиленого промісу.
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    return Promise.reject(errorMessage);
+  }
   // Викликаємо функцію fetchWithError
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  fetchWithError()
+    .then(() => {
+      console.log("22");
+    })
+    .catch((er) => {
+      console.error(er);
+    });
 }
 
 // Викликаємо функцію task10
